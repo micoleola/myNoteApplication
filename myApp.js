1445632d47c3130bb8f90fcf1b9cade9af55316a
Koleola <kmkuyoro@gmail.com>
2016-08-17 14:50:15 +0100

    Remodified my listNote function, added the getNote function

--- myApp.js
+++ myApp.js
@@ -1,31 +1,39 @@
+//create a note class
+class Note {
+	constructor(note, author) {
+		this.note = note;
+		this.author = author;
+	}
+}
+
 // create the NotesApplication class
 class NotesApplication {
-	constructor(author) {
-		this.author = author;
+	constructor() {
 		this.notes = [];
 	}
 //create note_content and add to note list
-	create(note_content) {
-		var note = new Note({
-			note_id: this.notes.length,
-			note_content: note_content,
-			author: this.author
-			});
+	create(note) {
+		if (note instanceof Note) {
 		this.notes.push(note);
+		}
 	}
 //list notes function	
 	listNotes() {
-		for(var note in this.notes){
-			console.log("Note ID:", note[note_id]);
-			console.log(note[note_content]);
-			console.log("Author by ", note[author])
+		for(var i = 0; i < this.notes.length; i++) {
+			console.log("Note ID:", i ,"\n", this.notes[i].note, "\n", "Author by ", this.notes[i].author, "\n");
 			}
 	}
+
 //get note function	
-	getNote(note_id){
-		var str = String(this.notes[note_id]);
-		return str;
-	}
+	getNote(note_id) {
+		if (note_id < this.notes.length && note_id > 0) {
+			console.log(this.notes[note_id].note, "\n");
+			}
+		else{
+			console.log("ID not found!!!")
+			}
+}
+	
 //	create the text search function
 	search(search_text){
 		for(var note in this.notes) {
@@ -35,21 +43,13 @@ class NotesApplication {
 				getNote(note[note_id]);			
 				console.log("By Author ", note[author])
 				}
-			}
+		}
+	
 //function to delete note
-	delete(note_id){
+//	deleteNote(note_id){
 		
-	}
-//edit new content by note index
-	edit(note_id, new_content) {
-		
+//		}
 	}
 }
-//create a note class
-class Note {
-	constructor(properties){
-		this.note_id = properties.note_id;
-		this.note_content = properties.note_content;
-		this.author = properties.author;
-	}
-}
+
+
