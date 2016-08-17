da4d8b9a6b280fb89379301e9b79031aa2d9f938
Koleola <kmkuyoro@gmail.com>
2016-08-17 12:22:26 +0100

    added listNotes function

--- myApp.js
+++ myApp.js
@@ -1,19 +1,20 @@
+// create the NotesApplication class
 class NotesApplication {
 	constructor(author) {
 		this.author = author;
 		this.notes = [];
 	}
-	
+//create note_content and add to note list
 	create(note_content) {
-		var note_content = 
 		this.notes.push(note_content)
 	}
-	
+//list notes function	
 	listNotes() {
-		for (var note in this.note){
-			note = {}
+		for (var note in this.notes){
+			console.log("Note ID:" notes.indexOf(note) "\n" note "\n" "By Author," this.author "\n")
+			}
 		}
-		
+	
 	}
 }
 
